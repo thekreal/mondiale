bd8dc35b3c8cd45a2c3a2b76fd20e06273a54af9
Scott Chu <domaru.tdot@gmail.com>
2014-03-04 17:53:30 -0500

    create start_date and end_date for trip.rb

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,9 +2,17 @@ class Trip < ActiveRecord::Base
 
 	belongs_to :user
 	has_many :chapters
-	#has_many :posts, :through => :chapters
+	has_many :posts, through: :chapters
 
   validates :title, :description, presence: true
   validates :title, uniqueness: true
+
+  def start_date
+    chapters.first.posts.first.date.to_date.to_formatted_s(:long)
+  end
+
+  def end_date
+    chapters.last.posts.last.date.to_formatted_s(:long)
+  end
 
 end
