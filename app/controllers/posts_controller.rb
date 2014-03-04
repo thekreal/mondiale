7f9efe90ab36fa322ec2033971742a940ba04949
Kevin Freeman <kevinfreeman@rogers.com>
2014-03-04 16:43:13 -0500

    creates posts forms, partials and new view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,20 @@
 class PostsController < ApplicationController
 
 	def new
+		@chapter = Chapter.find(params[:chapter_id])
+		@post = @chapter.posts.new()
 	end
 
 	def create
+
+		@post = Post.new(post_params)
+		# @post.chapter_id = @chapter
+		if @post.save
+			flash[:success] = "Your post has been created successfully"
+			redirect_to trip_chapter_path(@post.chapter.trip, @post.chapter)
+		else
+			render :new
+		end
 	end
 
 	def edit
@@ -18,7 +29,7 @@ class PostsController < ApplicationController
 private
 
 	def post_params
-		params.require(:post).permit(:title, :content, :date, )
+		params.require(:post).permit(:title, :content, :date, :chapter_id )
 	end
 
 end
