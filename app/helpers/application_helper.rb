ad58e429a3724347ead42747e1f6671db7b99524
Scott Chu <domaru.tdot@gmail.com>
2014-03-03 20:28:19 -0500

    Create titler helper method to generate site name and page name, update the application.html.erb view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  SITE_NAME = "Mondiale"
+
+  def site_name
+    return SITE_NAME
+  end
+
+  def titler(page_title)
+    page_title.empty? ? site_name : "#{site_name} | #{page_title}"
+  end
+
 end
