8bdc029500dbe56f0cdf3108269160893450933a
Scott Chu <domaru.tdot@gmail.com>
2014-03-04 18:28:49 -0500

    Add validations in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ActiveRecord::Base
 
 	belongs_to :chapter
+
+  # returns trip object when type post.trip
   delegate :trip, to: :chapter
 
-	validates :title, presence: true
+	validates :title,     presence: true
+  validates :content,   presence: true
+  validates :date,      presence: true
 
 end
