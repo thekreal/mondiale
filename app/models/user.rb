eefff3fba9cf8bb9bf29c8aebcf23506c736dcb5
Scott Chu <domaru.tdot@gmail.com>
2014-03-03 20:03:03 -0500

    Generated remember_me.rb and updated user model and created a users_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,30 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  #  Callbacks and Validations
+  before_save { email.downcase! }
+
+  validates :name,      presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,     presence: true,
+                        format: { with: VALID_EMAIL_REGEX },
+                        uniqueness: {
+                          case_sensitive: false,
+                          message: "has already being registered"
+                        }
+
+  validates :password,  length: { in: 6..32 },
+                        confirmation: true,
+                        exclusion: {
+                          in: lambda { |user| [user.email] },
+                          message: "can not be the same as email"
+                        }
+
+  # Retrieve Gravatar from gravatar.com using user email
+  def gravatar(size = 50)
+    return "https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(email)}?s=#{size}"
+  end
+
 end
+
