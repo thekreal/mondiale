6f23b794e2cac2fc9aea1a55caff1d8c6ade46ff
Kevin Freeman <kevinfreeman@rogers.com>
2014-03-05 18:08:56 -0500

    adds search bar to trip index, and activates search bar on landing page, when you type in a location it will now show all trip that contain a post near that location

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
 	belongs_to :chapter
 
   # returns trip object when type post.trip
-  # has_one :trip, through: :chapter
-  delegate :trip, to: :chapter
+  has_one :trip, through: :chapter
+  # delegate :trip, to: :chapter
 
 
 	validates :title,     presence: true
