2f5c0fdd40df4997937b6aae89069f48ca8ba03d
Scott Chu <domaru.tdot@gmail.com>
2014-03-05 14:40:56 -0500

    Update form style and form html tags

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   var alerts = $("[class*='alert']");
 
+  // if there is an alert
   if (alerts.length) {
+
+    // slide down to show the message
+    alerts.slideToggle(800, 'easeInOutQuad');
+
+    // when clicked on close button, slide up
     alerts.children('.close').click(function() {
-      $(this).parent().slideUp(800, 'easeOutBounce', function() {
+      $(this).parent().slideToggle(800, 'easeInOutQuad', function() {
         $(this).remove();
       })
     })
+
   }
 });
