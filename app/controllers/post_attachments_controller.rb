f96bdbf7eb9582352f9e33aa43018d9af715972f
David Yip <dvd.yip@gmail.com>
2014-03-06 13:36:12 -0500

    added delete picture functionality through ajax

--- app/controllers/post_attachments_controller.rb
+++ app/controllers/post_attachments_controller.rb
@@ -1,8 +1,8 @@
 class PostAttachmentsController < ApplicationController
 
-	# def destroy
-	# 	@post_attachment = PostAttachment.find(params[:id].to_i)
-	# 	@post_attachment.remove_postimage
-	# end
+	def destroy
+		@post_attachment = PostAttachment.find(params[:id].to_i)
+		@post_attachment.delete
+	end
 
 end
