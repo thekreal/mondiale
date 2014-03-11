96164e20dd8b0a1b53ad6a8e677d8c460f0e121e
Kevin Freeman <kevinfreeman@rogers.com>
2014-03-11 16:20:57 -0400

    adds methods to find if a user has added a model as an inspiration already, adds a method to find an inspiration based on the current user and the current model id and type

--- app/models/inspiration.rb
+++ app/models/inspiration.rb
@@ -2,5 +2,12 @@ class Inspiration < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :inspirable, polymorphic: true
 
+	def self.by_this(user_id, obj_id, obj_type)
+		where(user_id: user_id, inspirable_id: obj_id, inspirable_type: obj_type)
+	end
+
+	def self.by_this?(user_id, obj_id, obj_type)
+		where(user_id: user_id, inspirable_id: obj_id, inspirable_type: obj_type).exists?
+	end
 
 end
