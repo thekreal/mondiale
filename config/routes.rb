dbe4d5cea17d925f97ec5e256781c3d151ef54d5
Kevin Freeman <kevinfreeman@rogers.com>
2014-03-11 14:32:31 -0400

    adds a destroy route for inspirations

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Mondiale::Application.routes.draw do
   get 'signin'          =>      'sessions#new',             as: :signin
   get 'signout'         =>      'sessions#destroy',         as: :signout
   resources :sessions, only: [:create]
-  resources :inspirations, only: [:create]
+  resources :inspirations, only: [:create, :destroy]
 
   root 'welcome#index'
 end
