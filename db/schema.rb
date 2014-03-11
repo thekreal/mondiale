177d5efaf4ea849821a3618c42c277ef5c2432fd
Kevin Freeman <kevinfreeman@rogers.com>
2014-03-11 12:54:49 -0400

    creates the migration for inspirations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310233828) do
+ActiveRecord::Schema.define(version: 20140311161008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20140310233828) do
     t.string   "inspiration_type"
     t.integer  "inspiration_id"
     t.integer  "position"
+  end
+
+  create_table "inspirations", force: true do |t|
+    t.integer "user_id"
+    t.integer "inspirable_id"
+    t.string  "inspirable_type"
   end
 
   create_table "post_attachments", force: true do |t|
