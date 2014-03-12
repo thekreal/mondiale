bbe386e403b3137a04d9fd2c2891a4b555e89664
Scott Chu <domaru.tdot@gmail.com>
2014-03-12 17:56:23 -0400

    Update Chapter show page

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,8 +2,11 @@ class Chapter < ActiveRecord::Base
   include Common
 
   belongs_to :trip
+
   has_many :posts, dependent: :delete_all
+
   has_many :post_attachments, through: :posts
+
   has_many :inspirations, as: :inspirable
 
   acts_as_list scope: :trip
