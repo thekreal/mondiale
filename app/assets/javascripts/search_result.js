ad8d1ddc0266769ce221279724979b0a139fdaf6
Scott Chu <domaru.tdot@gmail.com>
2014-03-14 13:40:54 -0400

    Add search result

--- app/assets/javascripts/search_result.js
+++ app/assets/javascripts/search_result.js
@@ -1,10 +1,18 @@
 function searchResult() {
   var result = $('.search-result');
   if (result.length) {
+    $('.trips-index').outerHeight(windowHeight());
     result.each(function() {
-      console.log(this)
       $(this).outerHeight(windowHeight() / 2);
     })
 
   }
+}
+
+function smoothScroll(object, delay) {
+  $('html, body').delay(delay).stop().animate({
+    'scrollTop': object.offset().top
+  }, 1200, function() {
+    window.location.hash = object;
+  })
 }
