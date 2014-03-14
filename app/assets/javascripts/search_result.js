9b88f24bf4069c4be127b3b47de66e753ac7e30e
Scott Chu <domaru.tdot@gmail.com>
2014-03-14 13:51:47 -0400

    Update search result

--- app/assets/javascripts/search_result.js
+++ app/assets/javascripts/search_result.js
@@ -2,10 +2,15 @@ function searchResult() {
   var result = $('.search-result');
   if (result.length) {
     $('.trips-index').outerHeight(windowHeight());
-    result.each(function() {
-      $(this).outerHeight(windowHeight() / 2);
-    })
-
+    console.log(result.length)
+    if (result.length === 1) {
+      result.outerHeight(windowHeight());
+    }
+    else {
+      result.each(function() {
+        $(this).outerHeight(windowHeight() / 2);
+      })
+    }
   }
 }
 
