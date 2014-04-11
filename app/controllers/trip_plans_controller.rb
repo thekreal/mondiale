f3d8f18d0ca0ea2ffe259a621c93f522631a4c30
Kevin Freeman <kevinfreeman@rogers.com>
2014-04-10 21:21:34 -0400

    creates the method and the views for a new trip_plan

--- app/controllers/trip_plans_controller.rb
+++ app/controllers/trip_plans_controller.rb
@@ -1,6 +1,11 @@
 class TripPlansController < ApplicationController
 
 	def new
+		@trip_plan = current_user.trip_plans.new
+    respond_to do |format|
+      format.html
+      format.js
+    end
 	end
 
 	def show
