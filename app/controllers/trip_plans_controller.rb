98a86ae1215a13b95ec2a5e00c5c2d2b4ca833c6
Kevin Freeman <kevinfreeman@rogers.com>
2014-04-13 15:48:53 -0400

    creates the necessary javascript and changes in the controller to allow for the creation of new trip plans

--- app/controllers/trip_plans_controller.rb
+++ app/controllers/trip_plans_controller.rb
@@ -2,14 +2,26 @@ class TripPlansController < ApplicationController
 
 	def new
 		@trip_plan = current_user.trip_plans.new
+		respond_to do |format|
+			format.html
+			format.js
+		end
 	end
 
 	def create
-		@trip_plan = current_user.trips.new(trip_plan_params)
-		if @trip_plan.save
-			flash[:success] = "Your trip has been created successfully"
-      redirect_to @trip_plan
-    end
+		@trip_plan = current_user.trip_plans.new(trip_plan_params)
+		respond_to do |format|
+			if @trip_plan.save
+				format.html do
+				flash[:success] = "Your trip plan has been created successfully"
+	      redirect_to @trip_plan
+	    	end
+	    	format.js { trip_plan_path(@trip_plan) }
+	    else
+	    	format.html { render :new }
+	      format.js {}
+	    end
+		end
 	end
 
 	def show
@@ -19,7 +31,7 @@ class TripPlansController < ApplicationController
 private
 
 	def trip_plan_params
-    params.require(:trip).permit(:title, :description)
+    params.require(:trip_plan).permit(:title, :description)
   end
 
 end
