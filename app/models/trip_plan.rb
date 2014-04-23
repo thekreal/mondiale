315f5df7afc09aa52c570a63108e07e640a3b754
Kevin Freeman <kevinfreeman@rogers.com>
2014-04-23 17:15:25 -0400

    added geocoding to trip_plans

--- app/models/trip_plan.rb
+++ app/models/trip_plan.rb
@@ -3,6 +3,8 @@ class TripPlan < ActiveRecord::Base
 	has_many :plan_items
 	belongs_to :user
 
+	geocoded_by :location
+
 	validates :title,  presence: true,
 										 uniqueness: true
 	validates :description, presence: true
