3a9643e10e82f20c6ee29b73858a33353a7270af
Kevin Freeman <kevinfreeman@rogers.com>
2014-05-05 21:11:49 -0400

    fixes an issue with the creation of new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, except: [:index, :new, :edit]
+  before_action :set_user, except: [:index, :new, :edit, :create]
 
   def index
     @users = User.all
