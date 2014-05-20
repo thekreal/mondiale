8434f481ffe30462792d26ba8cbc4edd33fc6ac3
Kevin Freeman <kevinfreeman@rogers.com>
2014-05-19 21:52:39 -0400

    added delete functionality for trip plans

--- app/controllers/trip_plans_controller.rb
+++ app/controllers/trip_plans_controller.rb
@@ -1,5 +1,7 @@
 class TripPlansController < ApplicationController
 
+	before_action :set_trip_plan, only: [:show, :destroy]
+
 	def new
 		@trip_plan = current_user.trip_plans.new
 		respond_to do |format|
@@ -29,7 +31,17 @@ class TripPlansController < ApplicationController
 		@plan_items = @trip_plan.plan_items.trip_order
 	end
 
+	def destroy
+		@trip_plan.destroy
+		flash[:success] = "Your trip plan has been removed successfully"
+		redirect_to my_trip_plans_user_path(current_user)
+	end
+
 private
+
+	def set_trip_plan
+		@trip_plan = TripPlan.find(params[:id])
+	end
 
 	def trip_plan_params
     params.require(:trip_plan).permit(:title, :description)
