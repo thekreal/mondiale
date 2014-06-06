847bff62f138fa6135b83e0015248dce84bfea56
Kevin Freeman <kevinfreeman@rogers.com>
2014-06-06 18:53:44 -0400

    fixed untracked files

--- config/initializers/carrier_wave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'changed',                        # required
-    :aws_secret_access_key  => 'changed',                        # required
-  }
-  config.fog_directory  = 'mondiale'                     # required
-end
